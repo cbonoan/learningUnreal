bc83563ad85a536d55316c0d808c2ef51a96d747
Charles Bonoan <charlesbonoan@gmail.com>
2021-10-11 17:24:51 -0700

    continuing tutorial on unreal

--- BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,51 +5,78 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    // Setting up game
     InitGame();
 
-    // Welcome player 
-    PrintLine(TEXT("Welcome to Bull Cows!")); // Any string needs to be wrapped in the TEXT macro
-    PrintLine(TEXT("Guess the 4 letter word!")); // Magic number remove!
-    PrintLine(TEXT("Press ENTER to continue..."));
-
-    // Prompt player guess
+    //PrintLine(TEXT("The Hidden Word is: %s\nIt is %i characters long."), *HiddenWord, HiddenWord.Len()); // Debug Line 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
-    ClearScreen();
-
-    // Checking Player's guess
-
-    if(Input == HiddenWord) {
-        // Player wins
-        // Show how many guesses it took?
-        PrintLine(TEXT("That's the RIGHT word!"));
-
-        // Check if player wants to play again or quit
+{    
+    if(!bGameOver) {
+        ProcessGuess(Input);
+        
     } else {
-        // Deprecate from lives 
-        PrintLine(TEXT("That's the WRONG word!"));
+        ClearScreen();
+        InitGame();
     }
-
-    // Check if Isogram 
-        // Prompt to guess again
-    // Check right number of characters
-        // Prompt to guess again 
-    
-    // Remove life
-
-    // Check if lives > 0 
-    // if yes guess again and show remaining lives
-    // if no GameOver and HiddenWord
-        // prompt player to play again, Press ENTER to play again?
-        // Check user input 
-        // PlayAgain or Quit 
 }
 
 void UBullCowCartridge::InitGame() 
 {
+    // Welcome player 
+    PrintLine(TEXT("Welcome to Bull Cows!")); // Any string needs to be wrapped in the TEXT macro
+
     HiddenWord = TEXT("cake"); 
-    lives = 5; 
+    lives = HiddenWord.Len(); 
+    bGameOver = false; 
+
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // Magic number remove!
+    PrintLine(TEXT("You have %i lives. Good luck!"), lives);
+    PrintLine(TEXT("Enter your guess then\npress ENTER to continue..."));
+
+}
+
+void UBullCowCartridge::EndGame(bool correctGuess) 
+{
+    bGameOver = true; 
+    ClearScreen();
+    if(!correctGuess) {
+        PrintLine(TEXT("You have no more lives! Try again next time!"));
+        PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
+    } else {
+        PrintLine(TEXT("That's the RIGHT word! You WIN!"));
+    }
+    PrintLine(TEXT("Press ENTER to play again..."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess) 
+{
+    if(Guess == HiddenWord) {
+        EndGame(true); 
+        return;
+    }
+
+    // Check isogram
+    // if (!isIsogram) {
+    //    PrintLine(TEXT("No repeating letters, guess again.")); 
+    // }
+    
+    if(Guess.Len() != HiddenWord.Len()) {
+        PrintLine(TEXT("That's the wrong word!"));
+        PrintLine(TEXT("The Hidden Word is %i characters long. No lives will be taken away."), HiddenWord.Len());
+        return;
+    }
+    
+    --lives; 
+    if(lives > 1) {
+        PrintLine(TEXT("You lost a life! You now have %i lives!"), lives);
+    } else if(lives == 1){
+        PrintLine(TEXT("You lost a life! You now have %i life!!! Guess carefully..."), lives);
+    }
+    
+    if(lives == 0) {
+        EndGame(false);
+    }
+    
+    // Show player Bulls and Cows
 }
--- BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -15,9 +15,12 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 	void InitGame();
+	void EndGame(bool didWin);
+	void ProcessGuess(FString Guess);
 
 	// Your declarations go below!
 	private:
     FString HiddenWord; // Unreal uses FString type for strings 
 	int32 lives; 
+	bool bGameOver; 
 };
